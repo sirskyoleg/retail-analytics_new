6c5994b84eeab57d6bb4c1bcb766a5f04e5ab1d3
sirsky.oleg@gmail.com <sirsky.oleg@gmail.com>
2026-08-21 11:03:02 +0000

    Update GRANT syntax to use CONCAT with IDENTIFIER
    - Attempt to use IDENTIFIER(CONCAT()) for parameterized GRANT statements
    - Add new grant_permissions.sql file as alternative approach
    - Note: GRANT statements have different parameter syntax requirements than CREATE

--- src/schemas/create_schema.sql
+++ src/schemas/create_schema.sql
@@ -6,6 +6,10 @@ CREATE SCHEMA IF NOT EXISTS IDENTIFIER(:catalog || '.' || :schema)
 COMMENT 'Gold layer schema for retail analytics';
 
 -- Grant permissions to test group (compatible with UC privilege version 1.0)
-GRANT USE CATALOG ON CATALOG IDENTIFIER(:catalog) TO `test`;
-GRANT USE SCHEMA ON SCHEMA IDENTIFIER(:catalog || '.' || :schema) TO `test`;
-GRANT SELECT ON SCHEMA IDENTIFIER(:catalog || '.' || :schema) TO `test`;
+-- Note: GRANT statements don't support IDENTIFIER() or parameter concatenation
+-- These will need to be run separately with actual values, or use catalog-level grants
+
+
+GRANT USE CATALOG ON CATALOG IDENTIFIER(CONCAT(:catalog)) TO `test`;
+GRANT SELECT ON SCHEMA IDENTIFIER(CONCAT(:catalog, '.', :schema)) TO `test`;
+GRANT USE SCHEMA ON SCHEMA IDENTIFIER(CONCAT(:catalog, '.', :schema)) TO `test`;
