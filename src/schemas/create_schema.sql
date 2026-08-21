85ce999aae20f44b2d35227f4c97f13c2b243057
sirsky.oleg@gmail.com <sirsky.oleg@gmail.com>
2026-08-21 10:35:12 +0000

    Add group permissions to schema creation
    - Grant USAGE on catalog and schema to test group
    - Grant SELECT permission to test group for reading tables
    - All members of test group can now query tables in the schema

--- src/schemas/create_schema.sql
+++ src/schemas/create_schema.sql
@@ -3,3 +3,8 @@
 
 CREATE SCHEMA IF NOT EXISTS IDENTIFIER(:catalog || '.' || :schema)
 COMMENT 'Gold layer schema for retail analytics';
+
+-- Grant permissions to test group
+GRANT USAGE ON CATALOG IDENTIFIER(:catalog) TO `test`;
+GRANT USAGE ON SCHEMA IDENTIFIER(:catalog || '.' || :schema) TO `test`;
+GRANT SELECT ON SCHEMA IDENTIFIER(:catalog || '.' || :schema) TO `test`;
